7b9fc04ff210ab77dfda3712016f9d5f83655ee6
Lucas-M7 <lucas015x@gmail.com>
2024-02-15 16:45:57 -0300

    Up

---
+++ consumindoAPI/Program.cs
@@ -0,0 +1,33 @@
+﻿// Criar um cliente HTTP
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using var client = new HttpClient();
+
+// Definir a URL da API
+
+var url = "https://date.nager.at/api/v3/PublicHolidays/2024/BR";
+
+try
+{
+    // Fazer solicitação GET
+    HttpResponseMessage response = await client.GetAsync(url);
+
+    if (response.IsSuccessStatusCode)
+    {
+        string content = await response.Content.ReadAsStringAsync();
+
+        var conteudoIdentado = JToken.Parse(content).ToString();
+
+        Console.WriteLine(conteudoIdentado);
+    }
+    else
+    {
+        Console.WriteLine($"Erro ao processar o conteúdo: {response.StatusCode}");
+    }
+}
+catch
+{
+    Console.WriteLine($"Erro ao consumir a API");
+}
